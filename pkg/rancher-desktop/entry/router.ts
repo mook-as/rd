rancher-sandbox/rancher-desktop

commit ce63e32fd048d69f34e13572f301a89977f37797

UI: Remove nuxt
This removes the remenants of Nuxt that was kept around when we ejected
from Nuxt.

Notable changes:
- Nuxt had a `fetch` hook; since that no longer exists, do it on component
  before mount instead.
- We no longer have Nuxt layouts; fake it in the entry point.
- We need to init i18n in the layouts because the plugins don't get run.
- We need a different implementation of feature flags.

Signed-off-by: Mark Yen <mark.yen@suse.com>

pkg/rancher-desktop/entry/router.ts
@@ -1,0 +1,94 @@
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+
+import Containers from '../pages/Containers.vue';
+import DenyRoot from '../pages/DenyRoot.vue';
+import Diagnostics from '../pages/Diagnostics.vue';
+import Extensions from '../pages/Extensions.vue';
+import FirstRun from '../pages/FirstRun.vue';
+import General from '../pages/General.vue';
+import Images from '../pages/Images.vue';
+import KubernetesError from '../pages/KubernetesError.vue';
+import PortForwarding from '../pages/PortForwarding.vue';
+import Preferences from '../pages/Preferences.vue';
+import Snapshots from '../pages/Snapshots.vue';
+import SudoPrompt from '../pages/SudoPrompt.vue';
+import Troubleshooting from '../pages/Troubleshooting.vue';
+import UnmetPrerequisites from '../pages/UnmetPrerequisites.vue';
+import ExtensionsItem from '../pages/extensions/_root/_src/_id.vue';
+import ImagesAdd from '../pages/images/add.vue';
+import ImagesScan from '../pages/images/scans/_image-name.vue';
+import SnapshotsCreate from '../pages/snapshots/create.vue';
+import SnapshotsDialog from '../pages/snapshots/dialog.vue';
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  { path: '/', redirect: '/General' },
+  {
+    path: '/General', component: General, name: 'General',
+  },
+  {
+    path: '/Containers', component: Containers, name: 'Containers',
+  },
+  {
+    path: '/PortForwarding', component: PortForwarding, name: 'Port Forwarding',
+  },
+  {
+    path:      '/Images',
+    component: Images,
+    name:      'Images',
+  },
+  {
+    path: '/images/add', component: ImagesAdd, name: 'images-add',
+  },
+  {
+    path: '/images/scans/:image-name?/:namespace?', component: ImagesScan, name: 'images-scans-image-name',
+  },
+  {
+    path: '/Snapshots', component: Snapshots, name: 'Snapshots',
+  },
+  {
+    path: '/snapshots/create', component: SnapshotsCreate, name: 'snapshots-create',
+  },
+  {
+    path: '/Troubleshooting', component: Troubleshooting, name: 'Troubleshooting',
+  },
+  {
+    path: '/Diagnostics', component: Diagnostics, name: 'Diagnostics',
+  },
+  {
+    path: '/Extensions', component: Extensions, name: 'Extensions',
+  },
+  {
+    path: '/extensions/:id/:root(.*)/:src', component: ExtensionsItem, name: 'rdx-root-src-id',
+  },
+  {
+    path: '/DenyRoot', component: DenyRoot, name: 'DenyRoot',
+  },
+  {
+    path: '/FirstRun', component: FirstRun, name: 'FirstRun',
+  },
+  {
+    path: '/KubernetesError', component: KubernetesError, name: 'KubernetesError',
+  },
+  {
+    path: '/preferences', component: Preferences, name: 'Preferences',
+  },
+  {
+    path: '/SudoPrompt', component: SudoPrompt, name: 'SudoPrompt',
+  },
+  {
+    path: '/UnmetPrerequisites', component: UnmetPrerequisites, name: 'UnmetPrerequisites',
+  },
+  {
+    path: '/SnapshotsDialog', component: SnapshotsDialog, name: 'SnapshotsDialog',
+  },
+];
+
+const router = new VueRouter({
+  mode: 'hash',
+  routes,
+});
+
+export default router;

e2e/preferences.e2e.spec.ts
@@ -297,7 +297,7 @@ test.describe.serial('Main App Test', () => {
       expect(preferencesWindow).toBeDefined();
       // Wait for the window to actually load (i.e. transition from
       // app://index.html/#/preferences to app://index.html/#/Preferences#general)
-      await preferencesWindow.waitForURL(/Preferences/);
+      await preferencesWindow.waitForURL(/Preferences#/i);
       const { containerEngine } = new PreferencesPage(preferencesWindow);
 
       await expect(containerEngine.nav).toHaveClass('preferences-nav-item active');

pkg/rancher-desktop/components/DiagnosticsBody.vue
@@ -75,7 +75,7 @@ export default (Vue as VueConstructor<Vue & VuexBindings>).extend({
     },
 
     featureFixes(): boolean {
-      return !!this.$config.featureDiagnosticsFixes;
+      return !!process.env.RD_ENV_DIAGNOSTICS_FIXES;
     },
   },
   methods: {

pkg/rancher-desktop/components/Nav.vue
@@ -6,8 +6,8 @@
         :key="item.route"
         :item="item.route"
       >
-        <NuxtLink
-          :class="{'nuxt-link-active': isRouteActive(item.route) }"
+        <RouterLink
+          :class="{'rd-link-active': isRouteActive(item.route) }"
           :to="item.route"
         >
           {{ routes[item.route].name }}
@@ -25,13 +25,13 @@
             }"
             :class="`icon icon-flask`"
           />
-        </NuxtLink>
+        </RouterLink>
       </li>
     </ul>
     <hr v-if="extensionsWithUI.length">
     <div class="nav-extensions">
       <template v-for="extension in extensionsWithUI">
-        <nuxt-link
+        <RouterLink
           :key="extension.id"
           :data-test="`extension-nav-${ extension.metadata.ui['dashboard-tab'].title.toLowerCase() }`"
           :to="extensionRoute(extension)"
@@ -42,7 +42,7 @@
             </template>
             {{ extension.metadata.ui['dashboard-tab'].title }}
           </nav-item>
-        </nuxt-link>
+        </RouterLink>
       </template>
     </div>
     <div class="nav-button-container">
@@ -63,7 +63,6 @@
 <script lang="ts">
 import os from 'os';
 
-import { NuxtApp } from '@nuxt/types/app';
 import { BadgeState } from '@rancher/components';
 import Vue, { PropType } from 'vue';
 import { RouteRecordPublic } from 'vue-router';
@@ -73,6 +72,7 @@ import NavItem from './NavItem.vue';
 
 import DashboardButton from '@pkg/components/DashboardOpen.vue';
 import PreferencesButton from '@pkg/components/Preferences/ButtonOpen.vue';
+import router from '@pkg/entry/router';
 import type { ExtensionState } from '@pkg/store/extensions';
 import { hexEncode } from '@pkg/utils/string-encode';
 
@@ -93,8 +93,7 @@ export default Vue.extend({
       type:      Array as PropType<{route: string; error?: number; experimental?: boolean}[]>,
       required:  true,
       validator: (value: {route: string, error?: number}[]) => {
-        const nuxt: NuxtApp = (global as any).$nuxt;
-        const routes = nuxt.$router.getRoutes().reduce((paths: Record<string, RouteRecordPublic>, route) => {
+        const routes = router.getRoutes().reduce((paths: Record<string, RouteRecordPublic>, route) => {
           paths[route.path] = route;
 
           return paths;
@@ -117,12 +116,10 @@ export default Vue.extend({
     },
   },
   data() {
-    const nuxt: NuxtApp = (this as any).$nuxt;
-
     return {
       // Generate a route (path) to route entry mapping, so that we can pick out
       // their names based on the paths given.
-      routes: nuxt.$router.getRoutes().reduce((paths: Record<string, RouteRecordPublic>, route) => {
+      routes: this.$router.getRoutes().reduce((paths: Record<string, RouteRecordPublic>, route) => {
         paths[route.path] = route;
         if (route.name === 'Supporting Utilities' && os.platform() === 'win32') {
           route.name = 'WSL Integrations';
@@ -156,15 +153,13 @@ export default Vue.extend({
     },
     isRouteActive(route: string): boolean {
       // It is needed e.g. for sub-route /images/add not matching /Images
-      const nuxt: NuxtApp = (this as any).$nuxt;
-
       // Prevents the parent item "Extensions" to be shown as active if an extension child (e.g. Epinio, Logs Explorer,
       // ...) is selected.
-      if (nuxt.$route.name !== 'rdx-root-src-id') {
-        return nuxt.$route.path.split('/')[1] === route.substring(1).toLowerCase();
+      if (this.$route.name === 'rdx-root-src-id') {
+        return false;
       }
 
-      return false;
+      return this.$route.path.toLowerCase().startsWith(route.toLowerCase());
     },
     openPreferences(): void {
       this.$emit('open-preferences');
@@ -216,7 +211,7 @@ ul {
             outline: none;
         }
 
-        a.nuxt-link-active {
+        a:is(.router-link-active, .rd-link-active) {
             background-color: var(--nav-active);
         }
     }
@@ -227,7 +222,7 @@ a {
     text-decoration: none;
   }
 
-  &.nuxt-link-active::v-deep div {
+  &:is(.router-link-active, .rd-link-active)::v-deep div {
     background-color: var(--nav-active);
   }
 }

pkg/rancher-desktop/components/Preferences/BodyApplication.vue
@@ -30,9 +30,6 @@ export default Vue.extend({
       required: true,
     },
   },
-  async fetch() {
-    await this.$store.dispatch('credentials/fetchCredentials');
-  },
   computed: {
     ...mapGetters('preferences', ['isPlatformWindows']),
     ...mapGetters('transientSettings', ['getActiveTab']),
@@ -40,6 +37,9 @@ export default Vue.extend({
     activeTab(): string {
       return this.getActiveTab || 'behavior';
     },
+  },
+  async beforeMount() {
+    await this.$store.dispatch('credentials/fetchCredentials');
   },
   methods: {
     async tabSelected({ tab }: { tab: Vue.Component }) {

pkg/rancher-desktop/components/Tabbed/RdTabbed.vue
@@ -13,7 +13,6 @@ export default Vue.extend({
   <tabbed
     v-bind="$attrs"
     class="action-tabs"
-    :no-content="true"
     v-on="$listeners"
   >
     <slot name="tabs"></slot>

pkg/rancher-desktop/entry/index.ts
@@ -1,0 +1,37 @@
+/**
+ * This is the main entry point for Vue.
+ */
+
+import Cookies from 'cookie-universal';
+import Vue from 'vue';
+
+import router from './router';
+import store from './store';
+import '../plugins/clean-html-directive';
+import '../plugins/clean-tooltip-directive';
+import '../plugins/directives';
+import '../plugins/extend-router';
+import '../plugins/i18n';
+import '../plugins/shortkey';
+import '../plugins/tooltip';
+import '../plugins/trim-whitespace';
+import '../plugins/v-select';
+import '../plugins/vue-js-modal';
+
+Vue.config.productionTip = false;
+
+// This does just the Vuex part of cookie-universal-nuxt, which is all we need.
+(store as any).$cookies = Cookies();
+
+// Emulate Nuxt layouts by poking making the router match the main component we
+// will load, and then inspect it for the layout we set.
+// Because we're always using the hash mode for the router, get the correct
+// route based on the hash.
+const matched = router.match(location.hash.substring(1)).matched.find(r => r);
+const component = matched?.components.default as any;
+const layoutName: string = component?.extendOptions?.layout ?? 'default';
+const { default: layout } = await import(`../layouts/${ layoutName }.vue`);
+
+new Vue({
+  router, store, render: h => h(layout),
+}).$mount('#app');

pkg/rancher-desktop/entry/store.ts
@@ -1,0 +1,37 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import * as ActionMenu from '../store/action-menu';
+import * as ApplicationSettings from '../store/applicationSettings';
+import * as Credentials from '../store/credentials';
+import * as Diagnostics from '../store/diagnostics';
+import * as Extensions from '../store/extensions';
+import * as I18n from '../store/i18n';
+import * as ImageManager from '../store/imageManager';
+import * as K8sManager from '../store/k8sManager';
+import * as Page from '../store/page';
+import * as Preferences from '../store/preferences';
+import * as Prefs from '../store/prefs';
+import * as Snapshots from '../store/snapshots';
+import * as TransientSettings from '../store/transientSettings';
+
+Vue.use(Vuex);
+
+// eslint-disable-next-line import/no-named-as-default-member -- This is intended.
+export default new Vuex.Store<any>({
+  modules: {
+    'action-menu':       { namespaced: true, ...ActionMenu },
+    applicationSettings: { namespaced: true, ...ApplicationSettings },
+    credentials:         { namespaced: true, ...Credentials },
+    diagnostics:         { namespaced: true, ...Diagnostics },
+    extensions:          { namespaced: true, ...Extensions },
+    i18n:                { namespaced: true, ...I18n },
+    imageManager:        { namespaced: true, ...ImageManager },
+    k8sManager:          { namespaced: true, ...K8sManager },
+    page:                { namespaced: true, ...Page },
+    preferences:         { namespaced: true, ...Preferences },
+    prefs:               { namespaced: true, ...Prefs },
+    snapshots:           { namespaced: true, ...Snapshots },
+    transientSettings:   { namespaced: true, ...TransientSettings },
+  },
+});

mook-as/rd

commit 82baa0d8d6254ec3df5813fcb1d4b398ee41e9b8

Vue 3: Routes: add dialog route
This was missed in the Nuxt removal.

Fixes: ce63e32fd048d69f34e13572f301a89977f37797
Signed-off-by: Mark Yen <mark.yen@suse.com>

pkg/rancher-desktop/entry/router.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Containers from '../pages/Containers.vue';
 import DenyRoot from '../pages/DenyRoot.vue';
 import Diagnostics from '../pages/Diagnostics.vue';
+import Dialog from '../pages/Dialog.vue';
 import Extensions from '../pages/Extensions.vue';
 import FirstRun from '../pages/FirstRun.vue';
 import General from '../pages/General.vue';
@@ -73,6 +74,9 @@ export default createRouter({
     },
     {
       path: '/preferences', component: Preferences, name: 'Preferences',
+    },
+    {
+      path: '/Dialog', component: Dialog, name: 'Dialog',
     },
     {
       path: '/SudoPrompt', component: SudoPrompt, name: 'SudoPrompt',