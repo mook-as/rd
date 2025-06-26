rancher-sandbox/rancher-desktop

commit 9dba752f326b4c06fdd8dd149119071d1c6b3ecf

Change noproxy preference type to a list of string
Signed-off-by: Thomas Perale <thomas.perale@mind.be>

Resolved conflict with main
(https://github.com/rancher-sandbox/rancher-desktop/commit/8d70f99).

Conflicts:
            pkg/rancher-desktop/components/Preferences/WslProxy.vue

pkg/rancher-desktop/components/Preferences/WslProxy.vue
@@ -26,6 +26,14 @@ export default Vue.extend({
     isFieldDisabled() {
       return !(this.preferences.experimental.virtualMachine.proxy.enabled);
     },
+    noProxyText: {
+      get() {
+        return this.preferences.experimental.virtualMachine.proxy.noproxy.join('\n');
+      },
+      set(value: String) {
+        this.$store.dispatch('preferences/updatePreferencesData', { property: 'experimental.virtualMachine.proxy.noproxy', value: value.trim().split('\n') });
+      },
+    },
   },
   methods: {
     onChange<P extends keyof RecursiveTypes<Settings>>(property: P, value: RecursiveTypes<Settings>[P]) {
@@ -49,67 +57,83 @@ export default Vue.extend({
       />
     </rd-fieldset>
     <hr>
-    <rd-fieldset
-      data-test="addressTitle"
-      class="wsl-proxy-fieldset"
-      :legend-text="t('virtualMachine.proxy.addressTitle', { }, true)"
-    >
-      <rd-input
-        :placeholder="t('virtualMachine.proxy.address', { }, true)"
-        :disabled="isFieldDisabled"
-        :value="preferences.experimental.virtualMachine.proxy.address"
-        :is-locked="isPreferenceLocked('experimental.virtualMachine.proxy.address')"
-        class="wsl-proxy-field"
-        @input="onChange('experimental.virtualMachine.proxy.address', $event.target.value)"
-      />
-      <rd-input
-        type="number"
-        :placeholder="t('virtualMachine.proxy.port', { }, true)"
-        :disabled="isFieldDisabled"
-        :value="preferences.experimental.virtualMachine.proxy.port"
-        :is-locked="isPreferenceLocked('experimental.virtualMachine.proxy.port')"
-        class="wsl-proxy-field"
-        @input="onChange('experimental.virtualMachine.proxy.port', $event.target.value)"
-      />
-    </rd-fieldset>
-    <rd-fieldset
-      class="wsl-proxy-fieldset"
-      :legend-text="t('virtualMachine.proxy.authTitle', { }, true)"
-    >
-      <rd-input
-        :placeholder="t('virtualMachine.proxy.username', { }, true)"
-        :disabled="isFieldDisabled"
-        :value="preferences.experimental.virtualMachine.proxy.username"
-        :is-locked="isPreferenceLocked('experimental.virtualMachine.proxy.username')"
-        class="wsl-proxy-field"
-        @input="onChange('experimental.virtualMachine.proxy.username', $event.target.value)"
-      />
-      <rd-input
-        type="password"
-        :placeholder="t('virtualMachine.proxy.password', { }, true)"
-        :disabled="isFieldDisabled"
-        :value="preferences.experimental.virtualMachine.proxy.password"
-        :is-locked="isPreferenceLocked('experimental.virtualMachine.proxy.password')"
-        class="wsl-proxy-field"
-        @input="onChange('experimental.virtualMachine.proxy.password', $event.target.value)"
-      />
-    </rd-fieldset>
-    <rd-fieldset
-      class="wsl-proxy-fieldset"
-      :legend-text="t('virtualMachine.proxy.noproxyTitle', { }, true)"
-    >
-      <input
-        :placeholder="t('virtualMachine.proxy.noproxy', { }, true)"
-        :disabled="isFieldDisabled"
-        :value="preferences.experimental.virtualMachine.proxy.noproxy"
-        class="wsl-proxy-field"
-        @input="onChange('experimental.virtualMachine.proxy.noproxy', $event.target.value)"
-      />
-    </rd-fieldset>
+    <div class="proxy-row">
+      <div class="proxy-col">
+        <rd-fieldset
+          data-test="addressTitle"
+          class="wsl-proxy-fieldset"
+          :legend-text="t('virtualMachine.proxy.addressTitle', { }, true)"
+        >
+          <rd-input
+            :placeholder="t('virtualMachine.proxy.address', { }, true)"
+            :disabled="isFieldDisabled"
+            :value="preferences.experimental.virtualMachine.proxy.address"
+            :is-locked="isPreferenceLocked('experimental.virtualMachine.proxy.address')"
+            class="wsl-proxy-field"
+            @input="onChange('experimental.virtualMachine.proxy.address', $event.target.value)"
+          />
+          <rd-input
+            type="number"
+            :placeholder="t('virtualMachine.proxy.port', { }, true)"
+            :disabled="isFieldDisabled"
+            :value="preferences.experimental.virtualMachine.proxy.port"
+            :is-locked="isPreferenceLocked('experimental.virtualMachine.proxy.port')"
+            class="wsl-proxy-field"
+            @input="onChange('experimental.virtualMachine.proxy.port', $event.target.value)"
+          />
+        </rd-fieldset>
+        <rd-fieldset
+          class="wsl-proxy-fieldset"
+          :legend-text="t('virtualMachine.proxy.authTitle', { }, true)"
+        >
+          <rd-input
+            :placeholder="t('virtualMachine.proxy.username', { }, true)"
+            :disabled="isFieldDisabled"
+            :value="preferences.experimental.virtualMachine.proxy.username"
+            :is-locked="isPreferenceLocked('experimental.virtualMachine.proxy.username')"
+            class="wsl-proxy-field"
+            @input="onChange('experimental.virtualMachine.proxy.username', $event.target.value)"
+          />
+          <rd-input
+            type="password"
+            :placeholder="t('virtualMachine.proxy.password', { }, true)"
+            :disabled="isFieldDisabled"
+            :value="preferences.experimental.virtualMachine.proxy.password"
+            :is-locked="isPreferenceLocked('experimental.virtualMachine.proxy.password')"
+            class="wsl-proxy-field"
+            @input="onChange('experimental.virtualMachine.proxy.password', $event.target.value)"
+          />
+        </rd-fieldset>
+      </div>
+      <div class="proxy-col">
+        <rd-fieldset
+          :legend-text="t('virtualMachine.proxy.noproxyTitle', { }, true)"
+          :style="{ height: '100%' }"
+        >
+          <textarea
+            :placeholder="t('virtualMachine.proxy.noproxy', { }, true)"
+            :disabled="isFieldDisabled"
+            :value="preferences.experimental.virtualMachine.proxy.noproxy.join('\n')"
+            class="wsl-proxy-textarea"
+            @input="onChange('experimental.virtualMachine.proxy.noproxy', $event.target.value.split('\n'))"
+          />
+        </rd-fieldset>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+  .proxy-row {
+    display: flex;
+    flex-direction: row;
+  }
+  .proxy-col {
+    padding: 0rem .5rem;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+  }
   .wsl-proxy {
     display: flex;
     flex-direction: column;
@@ -122,5 +146,10 @@ export default Vue.extend({
   }
   .wsl-proxy-field {
     width: 100%;
+  }
+  .wsl-proxy-textarea{
+    width: 100%;
+    height: 100%;
+    padding: 2px;
   }
 </style>

pkg/rancher-desktop/assets/specs/command-api.yaml
@@ -544,8 +544,9 @@ components:
                       type: string
                       x-rd-usage: if needed the username to connect to the proxy
                     noproxy:
-                      type: string
-                      x-rd-usage: comma-separated list of addresses not redicrected to the proxy
+                      type: array
+                      x-rd-usage: list of hostname to exclude from using the proxy
+                      items: { type: string }
         WSL:
           type: object
           x-rd-platforms: [win32]

pkg/rancher-desktop/assets/translations/en-us.yaml
@@ -353,7 +353,7 @@ virtualMachine:
     username: Username
     password: Password
     noproxyTitle: No proxy hostname list
-    noproxy: Comma-seperated list of hostname to not redirect to the proxy
+    noproxy: List of hostname to not redirect to the proxy
   socketVmNet:
     legend: Enable socket-vmnet
     label: Use socket-vmnet instead of vde-vmnet

pkg/rancher-desktop/backend/wsl.ts
@@ -852,7 +852,7 @@ export default class WSLBackend extends events.EventEmitter implements VMBackend
       await this.writeFile(`/etc/moproxy/proxy.ini`, '; no proxy defined');
     }
 
-    await this.modifyConf('moproxy', { MOPROXY_NOPROXY: proxy.noproxy });
+    await this.modifyConf('moproxy', { MOPROXY_NOPROXY: proxy.noproxy.join(',') });
   }
 
   /**

pkg/rancher-desktop/config/settings.ts
@@ -127,7 +127,7 @@ export const defaultSettings = {
       /** windows only: if set, use gvisor based network rather than host-resolver/dnsmasq. */
       networkingTunnel: false,
       proxy:            {
-        enabled: false, address: '', password: '', port: 3128, username: '', noproxy: '0.0.0.0/8,10.0.0.0/8,127.0.0.0/8,169.254.0.0/16,172.16.0.0/12,192.168.0.0/16,224.0.0.0/4,240.0.0.0/4',
+        enabled: false, address: '', password: '', port: 3128, username: '', noproxy: ['0.0.0.0/8', '10.0.0.0/8', '127.0.0.0/8', '169.254.0.0/16', '172.16.0.0/12', '192.168.0.0/16', '224.0.0.0/4', '240.0.0.0/4'],
       },
     },
   },

pkg/rancher-desktop/main/commandServer/settingsValidator.ts
@@ -133,7 +133,7 @@ export default class SettingsValidator {
             password: this.checkString,
             port:     this.checkNumber(1, 65535),
             username: this.checkString,
-            noproxy:  this.checkString,
+            noproxy:  this.checkUniqueStringArray,
           },
         },
       },

mook-as/rd

commit d3e0791ac01ebdc53ef30812c6e37267b2ea3d50

prefs/WSLProxy: Drop unused noProxyText
This has never been used.

Signed-off-by: Mark Yen <mark.yen@suse.com>
Fixes: 9dba752f326b4c06fdd8dd149119071d1c6b3ecf

pkg/rancher-desktop/components/Preferences/WslProxy.vue
@@ -28,14 +28,6 @@ export default defineComponent({
     isFieldDisabled() {
       return !(this.preferences.experimental.virtualMachine.proxy.enabled);
     },
-    noProxyText: {
-      get() {
-        return this.preferences.experimental.virtualMachine.proxy.noproxy.join('\n');
-      },
-      set(value: string) {
-        this.$store.dispatch('preferences/updatePreferencesData', { property: 'experimental.virtualMachine.proxy.noproxy', value: value.trim().split('\n') });
-      },
-    },
     noproxyErrorMessages(): { duplicate: string } {
       return { duplicate: this.t('virtualMachine.proxy.noproxy.errors.duplicate') };
     },